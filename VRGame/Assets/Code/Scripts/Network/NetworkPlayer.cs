e2740204b21f13f9b8c6f70837523abbf08c183a
MatthijsDuits <93737737+MatthijsDuits@users.noreply.github.com>
2022-10-19 12:29:02 +0200

    Terug naar oude build

--- VRGame/Assets/Code/Scripts/Network/NetworkPlayer.cs
+++ VRGame/Assets/Code/Scripts/Network/NetworkPlayer.cs
@@ -10,11 +10,7 @@ public class NetworkPlayer : MonoBehaviour
 {
     public Transform head;
     public Transform leftHand;
-    public Transform rightHand;
-
-    //public Animator leftHandAnim;
-    //public Animator rightHandAnim;
-
+    public Transform rightHand; 
     public PhotonView photonView;
 
     private Transform headRig;
@@ -29,52 +25,26 @@ public class NetworkPlayer : MonoBehaviour
         headRig = rig.transform.Find("Camera Offset/Main Camera");
         leftHandRig = rig.transform.Find("Camera Offset/Left Hand");
         rightHandRig = rig.transform.Find("Camera Offset/Right Hand");
-
-/*        if(photonView.IsMine)
-        {
-            foreach (var item in GetComponentsInChildren<Renderer>())
-            {
-                item.enabled = false;
-            }
-        }*/
     }
 
     void Update()
     {
         if(photonView.IsMine)
         {
+            head.gameObject.SetActive(false);
+            leftHand.gameObject.SetActive(false);
+            rightHand.gameObject.SetActive(false);
+
             MapPosition(head, headRig);
             MapPosition(leftHand, leftHandRig);
             MapPosition(rightHand, rightHandRig);
-
-            //UpdateHandAnimation(InputDevices.GetDeviceAtXRNode(XRNode.LeftHand), leftHandAnim);
-            //UpdateHandAnimation(InputDevices.GetDeviceAtXRNode(XRNode.RightHand), rightHandAnim);
         }
     }
 
-/*    void UpdateHandAnimation(InputDevice targetDevice, Animator handAnimator)
-    {
-        if(targetDevice.TryGetFeatureValue(CommonUsages.trigger, out float triggerValue))
-        {
-            handAnimator.SetFloat("Trigger", triggerValue);
-        }
-        else
-        {
-            handAnimator.SetFloat("Trigger", 0);
-        }
-
-        if(targetDevice.TryGetFeatureValue(CommonUsages.grip, out float gripValue))
-        {
-            handAnimator.SetFloat("Grip", gripValue); 
-        }
-        else
-        {
-            handAnimator.SetFloat("Grip", 0);
-        }
-    }*/
-
     void MapPosition(Transform target, Transform rigTransform)
     { 
+
+
         target.position = rigTransform.position;
         target.rotation = rigTransform.rotation;
     }
