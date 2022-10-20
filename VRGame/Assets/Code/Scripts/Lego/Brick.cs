393d66a4d66fc3772c7c6df5a2add7415808babb
Nathan Diepeveen <nathan.n.diepeveen@student.hu.nl>
2022-10-20 09:32:29 +0200

    Lego Breaking Stuff

--- VRGame/Assets/Code/Scripts/Lego/Brick.cs
+++ VRGame/Assets/Code/Scripts/Lego/Brick.cs
@@ -4,22 +4,30 @@ using UnityEngine;
 
 public class Brick
 {
+   public float sizeX;
+    public float sizeZ;
+    public float height;
+    public GameObject cube { get; private set; }
+
     private LegoTools tools;
     private StudDictionary myStuds;
-    private GameObject cube;
-    private bool canBuild;
+    private bool canBuild, dropped;
 
-    public Brick(LegoTools _tools, float sizeX, float sizeY, float sizeZ, Material material, float height = 0.6f, bool _canBuild = true, bool canCollide = true)
+    public Brick(LegoTools _tools, float _sizeX, float _sizeZ, Material material, float _height = 0.6f, bool _canBuild = true, bool _dropped = false)
     {
+        sizeX = _sizeX;
+        sizeZ = _sizeZ;
+        height = _height;
         canBuild = _canBuild;
+        dropped = _dropped;
         tools = _tools;
         myStuds = new StudDictionary();
         cube = GameObject.CreatePrimitive(PrimitiveType.Cube);
 
-        if (!canCollide)
-            Object.DestroyImmediate(cube.GetComponent<Collider>());
+        if (dropped)
+            cube.AddComponent<Rigidbody>();
 
-        cube.transform.localScale = new Vector3(sizeX * 0.5f, sizeY * height, sizeZ * 0.5f) * tools.worldScale;
+        cube.transform.localScale = new Vector3(sizeX * 0.5f, height, sizeZ * 0.5f) * tools.worldScale;
 
         for (float x = 0; x < sizeX; x++)
         {
@@ -28,12 +36,11 @@ public class Brick
                 float fX = x / 2 - (sizeX / 4) + 0.25f;
                 float fZ = z / 2 - (sizeZ / 4) + 0.25f;
 
-                Vector3 studPos = new Vector3(fX, sizeY / 2 * height, fZ);
-
+                Vector3 studPos = new Vector3(fX, 0.5f * height, fZ);
                 GameObject newStud = tools.Clone(tools.stud, studPos);
                 newStud.transform.parent = cube.transform;
 
-                if (canBuild)
+                if (canBuild && !dropped)
                 {
                     tools.studs.Add(studPos, newStud);
                     myStuds.Add(studPos, newStud);
@@ -65,7 +72,7 @@ public class Brick
     {
         cube.transform.position = position * tools.worldScale;
 
-        if (canBuild)
+        if (canBuild && !dropped)
             foreach(KeyValuePair<Vector3, GameObject> v in myStuds.map)
             {
                 oldPosition = v.Key;
@@ -79,6 +86,16 @@ public class Brick
     public void SetPosition(float x, float y, float z)
     {
         SetPosition(new Vector3(x, y, z));
+    }
+
+    /// <summary>
+    /// Only works for dropped and non-build bricks.
+    /// Can be used for lerping
+    /// </summary>
+    public void SetAbsolutePosition(Vector3 position)
+    {
+        if (!canBuild || dropped)
+            cube.transform.position = position;
     }
 
     public void SetRotation(Quaternion rotation)
@@ -95,7 +112,7 @@ public class Brick
 
     public void BuildBrick(GameObject stud)
     {
-        if (!canBuild)
+        if (!canBuild || dropped)
             return;
             
         Vector3 studPos = stud.transform.position / tools.worldScale;
--- VRGame/Assets/Code/Scripts/Lego/LegoVRTools.cs
+++ VRGame/Assets/Code/Scripts/Lego/LegoVRTools.cs
@@ -1,47 +1,47 @@
-using System.Collections;
 using System.Collections.Generic;
-using UnityEngine;
+using UnityEngine.XR;
 
 public class LegoVRTools
 {
     // Prevent adding more garbage every frame
-    List<UnityEngine.XR.InputDevice> rightHandDevices, leftHandDevices;
-    UnityEngine.XR.InputDevice rightController, leftController;
-    bool rightGripPrevious, leftGripPrevious;
+    List<InputDevice> rightHandDevices, leftHandDevices;
+    InputDevice rightController, leftController;
 
-    public bool[] GetGripStates(bool detectPressOnly = true)
+    bool rightButtonPrevious, leftButtonPrevious;
+
+    public bool[] GetButtonStates(InputFeatureUsage<bool> inputFeature, bool detectPressOnly = true)
     {
-        bool rightGripPressed = false, leftGripPressed = false;
+        bool rightButtonPressed = false, leftButtonPressed = false;
 
         // Get the right and left hand devices
-        rightHandDevices = new List<UnityEngine.XR.InputDevice>();
-        leftHandDevices = new List<UnityEngine.XR.InputDevice>();
+        rightHandDevices = new List<InputDevice>();
+        leftHandDevices = new List<InputDevice>();
 
-        UnityEngine.XR.InputDevices.GetDevicesAtXRNode(UnityEngine.XR.XRNode.RightHand, rightHandDevices);
-        UnityEngine.XR.InputDevices.GetDevicesAtXRNode(UnityEngine.XR.XRNode.LeftHand, leftHandDevices);
+        InputDevices.GetDevicesAtXRNode(XRNode.RightHand, rightHandDevices);
+        InputDevices.GetDevicesAtXRNode(XRNode.LeftHand, leftHandDevices);
 
         // If a device is found, get it's gripButton state and set it
         if(rightHandDevices.Count > 0)
         {
             rightController = rightHandDevices[0];
-            rightController.TryGetFeatureValue(UnityEngine.XR.CommonUsages.gripButton, out rightGripPressed);
+            rightController.TryGetFeatureValue(inputFeature, out rightButtonPressed);
         }
 
         if(leftHandDevices.Count > 0)
         {
             leftController = leftHandDevices[0];
-            leftController.TryGetFeatureValue(UnityEngine.XR.CommonUsages.gripButton, out leftGripPressed);
+            leftController.TryGetFeatureValue(inputFeature, out leftButtonPressed);
         }
 
         bool[] result;
 
         if (detectPressOnly)
-            result = new bool[2] { leftGripPressed && !leftGripPrevious, rightGripPressed && !rightGripPrevious };
+            result = new bool[2] { leftButtonPressed && !leftButtonPrevious, rightButtonPressed && !rightButtonPrevious };
         else
-            result = new bool[2] { leftGripPressed, rightGripPressed };
+            result = new bool[2] { leftButtonPressed, rightButtonPressed };
 
-        rightGripPrevious = rightGripPressed;
-        leftGripPrevious = leftGripPressed;
+        rightButtonPrevious = rightButtonPressed;
+        leftButtonPrevious = leftButtonPressed;
 
         return result;
     }
